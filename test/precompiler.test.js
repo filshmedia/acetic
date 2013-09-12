5aabe23e86ea31d6d4153c25f05d1af8287cd24c
Sascha Gehlich <sascha@gehlich.us>
2013-09-12 23:06:45 +0200

    added precompilation tests for browserify and coffeeify

--- test/precompiler.test.js
+++ test/precompiler.test.js
@@ -6,61 +6,110 @@ var fs = require('fs')
   };
 
 describe('Precompiler', function() {
-  describe('per default', function () {
-    before(function (done) {
-      options.javascripts.files = [
-        'coffee/coffee.test.coffee'
-      ];
-      options.stylesheets.files = [
-        'stylus/stylus.test.styl'
-      ];
-      runPrecompiler(options, done);
+  describe('using coffee compiler', function () {
+    describe('if minify is disabled', function () {
+      before(function (done) {
+        options.javascripts.minify = false;
+        options.javascripts.files = [
+          'coffee/coffee.test.coffee'
+        ];
+        runPrecompiler(options, done);
+      });
+
+      it('should properly compile coffeescript files to javascript', function (done) {
+        expectFileEquality(
+          __dirname + '/app/public/assets/javascripts/coffee/coffee.test.js',
+          __dirname + '/fixtures/coffee/coffee.test.js'
+        );
+        done();
+      });
     });
 
-    it('should properly compile coffeescript files to javascript', function (done) {
-      expectFileEquality(
-        __dirname + '/app/public/assets/javascripts/coffee/coffee.test.js',
-        __dirname + '/fixtures/coffee/coffee.test.js'
-      );
-      done();
-    });
+    describe('if minify is enabled', function () {
+      before(function (done) {
+        options.javascripts.minify = true;
+        runPrecompiler(options, done);
+      });
 
-    it('should properly compile stylus files to css', function (done) {
-      expectFileEquality(
-        __dirname + '/app/public/assets/stylesheets/stylus/stylus.test.css',
-        __dirname + '/fixtures/stylus/stylus.test.css'
-      );
-      done();
+      it('should properly compile and minify coffeescript files to javascript', function (done) {
+        expectFileEquality(
+          __dirname + '/app/public/assets/javascripts/coffee/coffee.test.js',
+          __dirname + '/fixtures/coffee/coffee.test.minify.js'
+        );
+        done();
+      });
     });
   });
 
-  describe('if minify is enabled', function () {
-    before(function (done) {
-      options.javascripts.minify = true;
-      options.javascripts.files = [
-        'coffee/coffee.test.coffee'
-      ];
-      options.stylesheets.minify = true;
-      options.stylesheets.files = [
-        'stylus/stylus.test.styl'
-      ];
-      runPrecompiler(options, done);
+  describe('using browserify compiler', function () {
+    describe('if minify is disabled', function () {
+      before(function (done) {
+        options.javascripts.compiler = 'browserify';
+        options.javascripts.minify = false;
+        options.javascripts.files = [
+          'browserify/browserify.test.js'
+        ];
+        runPrecompiler(options, done);
+      });
+
+      it('should properly browserify javascript files', function (done) {
+        expectFileEquality(
+          __dirname + '/app/public/assets/javascripts/browserify/browserify.test.js',
+          __dirname + '/fixtures/browserify/browserify.test.js'
+        );
+        done();
+      });
     });
 
-    it('should properly minify the compiled javascripts', function (done) {
-      expectFileEquality(
-        __dirname + '/app/public/assets/javascripts/coffee/coffee.test.js',
-        __dirname + '/fixtures/coffee/coffee.test.minify.js'
-      );
-      done();
+    describe('if minify is enabled', function () {
+      before(function (done) {
+        options.javascripts.minify = true;
+        runPrecompiler(options, done);
+      });
+
+      it('should properly browserify and minify javascript files', function (done) {
+        expectFileEquality(
+          __dirname + '/app/public/assets/javascripts/browserify/browserify.test.js',
+          __dirname + '/fixtures/browserify/browserify.test.minify.js'
+        );
+        done();
+      });
+    });
+  });
+
+  describe('using coffeeify compiler', function () {
+    describe('if minify is disabled', function () {
+      before(function (done) {
+        options.javascripts.compiler = 'coffeeify';
+        options.javascripts.minify = false;
+        options.javascripts.files = [
+          'coffeeify/coffeeify.test.coffee'
+        ];
+        runPrecompiler(options, done);
+      });
+
+      it('should properly coffeeify javascript files', function (done) {
+        expectFileEquality(
+          __dirname + '/app/public/assets/javascripts/coffeeify/coffeeify.test.js',
+          __dirname + '/fixtures/coffeeify/coffeeify.test.js'
+        );
+        done();
+      });
     });
 
-    it('should properly minify the compiled stylesheets', function (done) {
-      expectFileEquality(
-        __dirname + '/app/public/assets/stylesheets/stylus/stylus.test.css',
-        __dirname + '/fixtures/stylus/stylus.test.minify.css'
-      );
-      done();
+    describe('if minify is enabled', function () {
+      before(function (done) {
+        options.javascripts.minify = true;
+        runPrecompiler(options, done);
+      });
+
+      it('should properly coffeeify and minify javascript files', function (done) {
+        expectFileEquality(
+          __dirname + '/app/public/assets/javascripts/coffeeify/coffeeify.test.js',
+          __dirname + '/fixtures/coffeeify/coffeeify.test.minify.js'
+        );
+        done();
+      });
     });
   });
 });
