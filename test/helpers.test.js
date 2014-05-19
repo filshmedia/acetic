1de2be26476bf8e03f1e16a8bb3954f2935c74c6
Sascha Gehlich <sascha@gehlich.us>
2014-05-19 15:59:25 +0200

    Allow additional attributes in helpers

--- test/helpers.test.js
+++ test/helpers.test.js
@@ -21,13 +21,22 @@ describe('Template helpers', function () {
         var output = app.acetic.helper.use('javascripts')('application.js');
         output.should.equal('<script src="/assets/javascripts/application.js"></script>');
       });
+
       it('should generate multiple script tags when multiple paths are given', function () {
         var output = app.acetic.helper.use('javascripts')('test.js', 'application.js');
         output.should.equal('<script src="/assets/javascripts/test.js"></script><script src="/assets/javascripts/application.js"></script>');
       });
+
       it('should render a url instead of a path if a url is given', function () {
         var output = app.acetic.helper.use('javascripts')('http://google.com/test.js');
         output.should.equal('<script src="http://google.com/test.js"></script>');
+      });
+
+      describe('if the second argument is not a string', function () {
+        it('should use it to render additional attributes', function () {
+          var output = app.acetic.helper.use('javascripts')('test.js', { 'data-main': 'foobarbaz!' });
+          output.should.equal('<script src="/assets/javascripts/test.js" data-main="foobarbaz!"></script>');
+        });
       });
     });
 
@@ -36,10 +45,12 @@ describe('Template helpers', function () {
         var output = app.acetic.helper.use('stylesheets')('application.css');
         output.should.equal('<link rel="stylesheet" href="/assets/stylesheets/application.css" />');
       });
+
       it('should generate multiple meta tags when multiple paths are given', function () {
         var output = app.acetic.helper.use('stylesheets')('reset.cs', 'application.css');
         output.should.equal('<link rel="stylesheet" href="/assets/stylesheets/reset.cs" /><link rel="stylesheet" href="/assets/stylesheets/application.css" />');
       });
+
       it('should render a url instead of a path if a url is given', function () {
         var output = app.acetic.helper.use('stylesheets')('http://google.com/test.css');
         output.should.equal('<link rel="stylesheet" href="http://google.com/test.css" />');
